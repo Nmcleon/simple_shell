4f8789f0b79b3eb2c8984813a11076b1055081ab
N <51336368+Nmcleon@users.noreply.github.com>
2023-07-14 13:22:38 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,19 +1,35 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #define MAX_COMMAND_LENGTH 1024
-#define MAX_ARGUMENTS 100
+#define MAX_ARGS 64
+#define MAX_PATH_LENGTH 1024
 
-typedef struct {
-    char command[MAX_COMMAND_LENGTH];
-    char arguments[MAX_ARGUMENTS][MAX_COMMAND_LENGTH];
-    int num_arguments;
-} Command;
+extern char **environ;
 
-void readCommand(Command *command);
-void executeBuiltInCommand(Command *command);
-void executeExternalCommand(Command *command);
-void freeCommand(Command *command);
-void shell_loop();
+/* Function Declarations */
+void display_prompt(void);
+void read_command(char *command);
+void parse_command(char *command, char **args);
+int execute_command(char **args);
+void handle_exit(void);
+void handle_env(void);
+void handle_setenv(char **args);
+void handle_unsetenv(char **args);
+void handle_cd(char **args);
+void handle_alias(char **args);
+void handle_logical_operators(char **args, int *result);
+void handle_command_separator(char *command);
+void handle_redirection(char **args);
 
-#endif
+char *get_full_path(char *command);
+void expand_variables(char **args);
+void get_variable_value(char *variable, char *value);
+bool is_variable(char *arg);
+void substitute_variable(char *arg);
+
+#endif /* SHELL_H */
