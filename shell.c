dd2954fbcd6a3dfe065c3d2d1eed19c66a183fc9
N <51336368+Nmcleon@users.noreply.github.com>
2023-07-14 12:47:25 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,4 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #include "shell.h"
+
+void readCommand(Command *command) {
+    printf("Enter a command: ");
+    fgets(command->command, sizeof(command->command), stdin);
+    command->command[strcspn(command->command, "\n")] = '\0';  // Remove trailing newline character
+
+    char *token = strtok(command->command, " ");
+    command->num_arguments = 0;
+
+    while (token != NULL && command->num_arguments < MAX_ARGUMENTS) {
+        strcpy(command->arguments[command->num_arguments++], token);
+        token = strtok(NULL, " ");
+    }
+}
 
 void executeBuiltInCommand(Command *command) {
     if (strcmp(command->command, "cd") == 0) {
@@ -11,13 +32,12 @@ void executeBuiltInCommand(Command *command) {
 void executeExternalCommand(Command *command) {
     pid_t pid = fork();
     if (pid == 0) {
-        char *args[MAX_COMMAND_ARGS];
+        char *args[MAX_ARGUMENTS + 2];
         args[0] = command->command;
-        int i;
-        for (i = 0; i < command->argCount; i++) {
+        for (int i = 0; i < command->num_arguments; i++) {
             args[i + 1] = command->arguments[i];
         }
-        args[command->argCount + 1] = NULL;
+        args[command->num_arguments + 1] = NULL;
 
         execvp(command->command, args);
         perror("execvp");
@@ -33,46 +53,31 @@ void executeExternalCommand(Command *command) {
 void freeCommand(Command *command) {
     // Free command resources implementation
     // ...
+    (void)command; // Silence unused parameter warning
 }
 
 void shell_loop() {
-    char input[256];
     Command command;
-    char *token;
 
     while (1) {
-        printf("$ "); // or shell> if you prefer
-        fgets(input, sizeof(input), stdin);
+        readCommand(&command);
 
-        // Tokenize the input
-        token = strtok(input, " \n");
-        if (token == NULL) {
-            continue; // Empty command, prompt again
+        if (strcmp(command.command, "") == 0) {
+            continue;
         }
 
-        // Extract the command
-        strcpy(command.command, token);
-
-        // Extract the arguments
-        command.argCount = 0;
-        while ((token = strtok(NULL, " \n")) != NULL) {
-            strcpy(command.arguments[command.argCount], token);
-            command.argCount++;
-            if (command.argCount >= MAX_COMMAND_ARGS) {
-                break;
-            }
-        }
-
-        // Execute the command
-        if (strcmp(command.command, "exit") == 0) {
-            break; // Exit the shell
-        } else if (strcmp(command.command, "cd") == 0) {
+        if (strcmp(command.command, "help") == 0) {
+            printf("This is a simple shell program.\n");
+            printf("Supported commands:\n");
+            printf("- exit: Exit the shell\n");
+            printf("- cd [directory]: Change the current directory\n");
+            printf("- help: Display this help message\n");
+        } else if (strcmp(command.command, "cd") == 0 || strcmp(command.command, "exit") == 0) {
             executeBuiltInCommand(&command);
         } else {
             executeExternalCommand(&command);
         }
     }
 
-    // Clean up and exit
     freeCommand(&command);
 }
