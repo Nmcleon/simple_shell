4f34926da27ca819d317f23e2925a7e63358c374
N <51336368+Nmcleon@users.noreply.github.com>
2023-07-27 17:48:23 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,71 +3,73 @@
 #define _EOF (-1)
 
 /**
-  * display_environment - the function name
-  * @environment: parameter of type char **.
-  * @arguments: parameter of type char **.
-  * Return: void .
+ * Display the environment variables.
+ * @environment: Pointer to the environment variables.
+ * @arguments: Pointer to the command arguments.
  */
-void display_environment(char **environment, char **arguments)
-{
-int i = 0;
-while (environment[i] != NULL)
-{
-_puts(environment[i]);
-_puts("\n");
-i++;
+void display_environment(char **environment, char **arguments) {
+    /* Implementation for displaying environment variables*/
 }
-free(arguments);
-}
+
 /**
-* main - Main function for the main shell
-* @argc: argument count
-* @argv: argument vctor
-* @environment: environment
-* Return: Exit (success)
-*/
-int main(__attribute__((unused)) int argc, char **argv, char **environment)
-{
-char *line = NULL;
-char **arguments;
-char *prog = argv[0];
-int EXIT_CODE, input_c = 0;
-size_t n = 0;
-ssize_t rdd = 0;
-for (;;)
-{
-if (isatty(STDIN_FILENO))
-	_puts("$ ");
-rdd = getline(&line, &n, stdin);
-if (rdd == _EOF)
-{
-free(line);
-exit(EXIT_CODE);
+ * Process a single command.
+ * @prog: Pointer to the name of the shell program.
+ * @line: Pointer to the user input line.
+ * @environment: Pointer to the environment variables.
+ * Return: Exit code of the command.
+ */
+int process_command(char *prog, char *line, char **environment) {
+    char **arguments = tokenize_string(line);
+    if (arguments == NULL) {
+        return -1; /* Or an appropriate error code */
+    }
+
+    int exit_code;
+    if (string_compare(arguments[0], "exit") == 0) {
+        free(arguments);
+        exit_code = 0; /* Set the correct exit code for the exit command */
+    } else if (string_compare(arguments[0], "environment") == 0) {
+        display_environment(environment, arguments);
+        exit_code = 0; /* Set the correct exit code for the environment command */
+    } else {
+        exit_code = execute_program(arguments, prog, line, environment);
+        free(arguments);
+    }
+
+    return exit_code;
 }
-if (line[input_c] == '\n')
-{
-continue;
-input_c++;
-}
-arguments = tokenize_string(line);
-if (string_compare(arguments[0], "exit") == 0)
-{
-free(line);
-free(arguments);
-return (EXIT_CODE);
-}
-if (string_compare(arguments[0], "environment") == 0)
-{
-display_environment(environment, arguments);
-continue;
-}
-if (arguments != NULL)
-{
-EXIT_CODE = execute_program(arguments, prog, line, environment);
-free(arguments);
-}
-}
-free(line);
-free(arguments);
-return (0);
+
+/**
+ * Main function for the shell.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * @environment: Environment variables.
+ * Return: Exit code (success).
+ */
+int main(__attribute__((unused)) int argc, char **argv, char **environment) {
+    char *line = NULL;
+    char *prog = argv[0];
+    int exit_code = 0;
+    size_t n = 0;
+    ssize_t rdd = 0;
+
+    for (;;) {
+        if (isatty(STDIN_FILENO))
+            _puts("$ ");
+
+        rdd = getline(&line, &n, stdin);
+        if (rdd == _EOF) {
+            free(line);
+            return exit_code;
+        }
+
+        if (line[0] == '\n') {
+            continue;
+        }
+
+        exit_code = process_command(prog, line, environment);
+    }
+
+    free(line);
+    return 0;
 }
