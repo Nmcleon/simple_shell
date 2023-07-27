9e3b78278dae08f575f6aa0442e2c1dcc4dfe059
Nmcleon <nmcleon10@gmail.com>
2023-07-27 08:32:43 -0700

    betty

--- execprog.c
+++ execprog.c
@@ -1,40 +1,39 @@
 #include "main.h"
 
 /**
- * execute_program - fork exe and wait for function
- * @arguments: pointer of tokens.
- * @prog: point to shell name
- * @lines: point to user input
- * @environment: ptr to environment vector
- * Return: integers;
- */
-
-int execute_program(char *arguments[], char *prog, char *lines, char **environment)
+* execute_program - the function name
+* @arguments: parameter of type char *.
+* @prog: parameter of type char *.
+* @lines: parameter of type char *.
+* @environment: parameter of type char **.
+* Return: int .
+*/
+int execute_program(char *arguments[], char *prog,
+		char *lines, char **environment)
 {
-    pid_t pid = fork();
-    char *command = arguments[0];
-    int state = 0;
-    int EXIT = 0;
+	pid_t pid = fork();
+	char *command = arguments[0];
+	int state = 0;
+	int EXIT = 0;
 
-    if (pid < 0)
-    {
-        perror(prog);
-        _exit(-EXIT_FAILURE);
-    }
-    else if (pid == 0) /* if child process */
-    {
-        if ((execve(command, arguments, environment) == -1)) /* only returns on error */
-        {
-            execution_error(command); /* perror if command can't execute */
-            free(arguments);
-            free(lines);
-            _exit(-126);
-        }
-        _exit(EXIT_SUCCESS);
-    }
-    /* wait for terminated children */
-
-    waitpid(pid, &state, WEXITSTATUS(state)); /* Return exit status of child */
-    EXIT = WEXITSTATUS(state);
-    return (EXIT); /* Return EXIT */
+	if (pid < 0)
+	{
+		perror(prog);
+		_exit(-EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if ((execve(command, arguments, environment) == -1))
+		{
+			execution_error(command);
+			free(arguments);
+			free(lines);
+			_exit(-126);
+		}
+		_exit(EXIT_SUCCESS);
+	}
+/* wait for terminated children */
+	waitpid(pid, &state, WEXITSTATUS(state));
+	EXIT = WEXITSTATUS(state);
+	return (-EXIT);
 }
