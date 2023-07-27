d8de6c68de466ed625536d33762083a7edebd508
Nmcleon <nmcleon10@gmail.com>
2023-07-27 08:53:46 -0700

    betty

--- shell.c
+++ shell.c
@@ -3,73 +3,81 @@
 #define _EOF (-1)
 
 /**
- * Display the environment variables.
- * @environment: Pointer to the environment variables.
- * @arguments: Pointer to the command arguments.
- */
-void display_environment(char **environment, char **arguments) {
-    /* Implementation for displaying environment variables*/
+* Display the environment variables.
+* @environment: Pointer to the environment variables.
+* @arguments: Pointer to the command arguments.
+*/
+void display_environment(char **environment, char **arguments)
+{
+/* Implementation for displaying environment variables*/
 }
-
 /**
- * Process a single command.
- * @prog: Pointer to the name of the shell program.
- * @line: Pointer to the user input line.
- * @environment: Pointer to the environment variables.
- * Return: Exit code of the command.
- */
-int process_command(char *prog, char *line, char **environment) {
-    char **arguments = tokenize_string(line);
-    if (arguments == NULL) {
-        return -1; /* Or an appropriate error code */
-    }
+* Process a single command.
+* @prog:
+* @line:
+* @environment:
+* Return:
+*/
+int process_command(char *prog, char *line, char **environment)
+{
+	char **arguments = tokenize_string(line);
 
-    int exit_code;
-    if (string_compare(arguments[0], "exit") == 0) {
-        free(arguments);
-        exit_code = 0; /* Set the correct exit code for the exit command */
-    } else if (string_compare(arguments[0], "environment") == 0) {
-        display_environment(environment, arguments);
-        exit_code = 0; /* Set the correct exit code for the environment command */
-    } else {
-        exit_code = execute_program(arguments, prog, line, environment);
-        free(arguments);
-    }
+	if (arguments == NULL)
+	{
+		return (-1);
+	}
+	int exit_code;
 
-    return exit_code;
+	if (string_compare(arguments[0], "exit") == 0)
+	{
+		free(arguments);
+		exit_code = 0;
+	}
+	else if (string_compare(arguments[0], "environment") == 0)
+	{
+		display_environment(environment, arguments);
+		exit_code = 0;
+	}
+	else
+	{
+		exit_code = execute_program(arguments, prog, line, environment);
+		free(arguments);
+	}
+	return (exit_code);
 }
-
 /**
- * Main function for the shell.
- * @argc: Argument count.
- * @argv: Argument vector.
- * @environment: Environment variables.
- * Return: Exit code (success).
- */
-int main(__attribute__((unused)) int argc, char **argv, char **environment) {
-    char *line = NULL;
-    char *prog = argv[0];
-    int exit_code = 0;
-    size_t n = 0;
-    ssize_t rdd = 0;
+* Main function for the shell.
+* @argc:
+* @argv:
+* @environment:
+* Return: Exit code (success)
+*/
+int main(__attribute__((unused)) int argc, char **argv, char **environment)
+{
+	char *line = NULL;
+	char *prog = argv[0];
+	int exit_code = 0;
+	size_t n = 0;
+	ssize_t rdd = 0;
 
-    for (;;) {
-        if (isatty(STDIN_FILENO))
-            _puts("$ ");
-
-        rdd = getline(&line, &n, stdin);
-        if (rdd == _EOF) {
-            free(line);
-            return exit_code;
-        }
-
-        if (line[0] == '\n') {
-            continue;
-        }
-
-        exit_code = process_command(prog, line, environment);
-    }
-
-    free(line);
-    return 0;
+	for (;;)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_puts("$ ");
+		}
+		rdd = getline(&line, &n, stdin);
+		if (rdd == _EOF)
+		{
+			free(line);
+			return (exit_code);
+		}
+		if (line[0] == '\n')
+		{
+			continue;
+		}
+		exit_code = process_command(prog, line, environment);
+	}
+	free(line);
+	return (0);
 }
--- stread.c
+++ stread.c
@@ -9,45 +9,49 @@
 */
 int argument_length(char *lines)
 {
-int args = 1;
-int i = 0;
-int cmnd = 0;
-while (lines[i] != '\0' && lines[i] != '\n')
-{
-if (lines[i] != ' ')
-	cmnd = 1;
+	int args = 1;
+	int i = 0;
+	int cmnd = 0;
+
+	while (lines[i] != '\0' && lines[i] != '\n')
+	{
+		if (lines[i] != ' ')
+		{
+			cmnd = 1;
+		}
 if (lines[i] == ' ' && lines[i + 1] != ' '
-&& lines[i + 1] != '\n' && cmnd == 1)
-args++;
-i++;
-}
-return (args);
+		&& lines[i + 1] != '\n' && cmnd == 1)
+		args++;
+		i++;
+	}
+	return (args);
 }
 /**
 * tokenize_string - function splits the command line input into tokens
-* @lines: tokenized string
+* @lines:
 * Return: null(success)
 */
 char **tokenize_string(char *lines)
 {
-int i = 0;
-int b_size;
-char **arguments;
-char *token;
-b_size = argument_length(lines);
-arguments = malloc((b_size + 1) * sizeof(char *));
-if (arguments == NULL)
-{
-free(arguments);
-return (NULL);
-}
-token = strtok(lines, DELIM);
-while (token != NULL && i <= b_size)
-{
-arguments[i] = token;
-i++;
-token = strtok(NULL, DELIM);
-}
-arguments[i] = NULL;
-return (arguments);
+	int i = 0;
+	int b_size;
+	char **arguments;
+	char *token;
+
+	b_size = argument_length(lines);
+	arguments = malloc((b_size + 1) * sizeof(char *));
+	if (arguments == NULL)
+	{
+		free(arguments);
+		return (NULL);
+	}
+	token = strtok(lines, DELIM);
+	while (token != NULL && i <= b_size)
+	{
+		arguments[i] = token;
+		i++;
+		token = strtok(NULL, DELIM);
+	}
+	arguments[i] = NULL;
+	return (arguments);
 }
